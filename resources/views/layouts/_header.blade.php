63c7b481aa0a9d52f1397c8340b759239f58afa2
zw896 <wzz343@gmail.com>
2018-11-15 16:54:52 +1100

    Add captcha

--- resources/views/layouts/_header.blade.php
+++ resources/views/layouts/_header.blade.php
@@ -1,49 +1,54 @@
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
- <a class="navbar-brand" href="#">
-    <img src="/docs/4.1/assets/brand/bootstrap-solid.svg" width="30" height="30" class="d-inline-block align-top" alt="">
-    Bootstrap
-</a>
+<!-- Navigation -->
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark static-top">
+  <div class="container">
+    <a class="navbar-brand" href="#">
+      <img src="http://placehold.it/150x50?text=Logo" alt="">
+    </a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarResponsive">
+      <ul class="navbar-nav ml-auto">
+        <li class="nav-item active">
+          <a class="nav-link" href="{{ url('/') }}">Home
+            <span class="sr-only">(current)</span>
+          </a>
+        </li>
 
-<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-</button>
-<div class="collapse navbar-collapse" id="navbarNav">
-    <ul class="navbar-nav">
-      <li class="nav-item active">
-        <a class="nav-link" href="{{ url('/') }}">Home <span class="sr-only">(current)</span></a>
-    </li>
-    @guest
-    <li class="nav-item">
-        <a class="nav-link" href="{{ route('register') }}">Sign up</a>
-    </li>
-    <li class="nav-item">
-        <a class="nav-link" href="{{ route('login') }}">Login</a>
-    </li>
-    @else
-    <li class="dropdown">
-        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
+        <!-- Authentication Links -->
+        @guest
+
+        <li class="nav-item">
+          <a class="nav-link" href="{{ url('/login') }}">Login</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="{{ url('/register') }}">Signup</a>
+        </li>
+        @else
+        <li class="dropdown">
+          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
             <span class="user-avatar pull-left" style="margin-right:8px; margin-top:-5px;">
-                <img src="https://iocaffcdn.phphub.org/uploads/images/201709/20/1/PtDKbASVcz.png?imageView2/1/w/60/h/60" class="img-responsive img-circle" width="30px" height="30px">
+              <img src="https://iocaffcdn.phphub.org/uploads/images/201709/20/1/PtDKbASVcz.png?imageView2/1/w/60/h/60" class="img-responsive img-circle" width="30px" height="30px">
             </span>
             {{ Auth::user()->name }} <span class="caret"></span>
-        </a>
+          </a>
 
-        <ul class="dropdown-menu" role="menu">
+          <ul class="dropdown-menu" role="menu">
             <li>
-                <a href="{{ route('logout') }}"
-                onclick="event.preventDefault();
-                document.getElementById('logout-form').submit();">
-                退出登录
+              <a href="{{ route('logout') }}"
+              onclick="event.preventDefault();
+              document.getElementById('logout-form').submit();">
+              退出登录
             </a>
 
             <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                {{ csrf_field() }}
+              {{ csrf_field() }}
             </form>
-            </li>
+          </li>
         </ul>
-    </li>
-    @endguest
-
-</ul>
+      </li>
+      @endguest
+    </ul>
+  </div>
 </div>
 </nav>
--- resources/views/pages/root.blade.php
+++ resources/views/pages/root.blade.php
@@ -2,5 +2,10 @@
 @section('title', '首页')
 
 @section('content')
-  <h1>这里是首页</h1>
+  <!-- Page Content -->
+<div class="container">
+  <h1 class="mt-4">Logo Nav by Start Bootstrap</h1>
+  <p>The logo in the navbar is now a default Bootstrap feature in Bootstrap 4! Make sure to set the width and height of the logo within the HTML or with CSS. For best results, it's recommended that you use an SVG image as your logo.</p>
+</div>
+<!-- /.container -->
 @stop
