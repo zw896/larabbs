51e26033ee8f1091ab85e92d7c6e108dbf719f55
Billy <billy@globaltllc.com>
2019-02-19 07:32:21 +1100

    登录成功后导航逻辑

--- resources/views/layouts/_header.blade.php
+++ resources/views/layouts/_header.blade.php
@@ -49,6 +49,7 @@
 
             <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
               {{ csrf_field() }}
+              {{ method_field('DELETE') }}
             </form>
           </li>
 
