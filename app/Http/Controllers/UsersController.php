63304184fa909331af7d79da41e53cc0634d0189
Billy <billy@globaltllc.com>
2019-02-20 23:07:54 +1100

    9.4 mail

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -96,13 +96,12 @@ class UsersController extends Controller
     {
         $view = 'emails.confirm';
         $data = compact('user');
-        $from = 'wzz343@gmail.com';
-        $name = 'WZZ343';
+
         $to = $user->email;
         $subject = "感谢注册 Weibo 应用！请确认你的邮箱。";
 
-        Mail::send($view, $data, function ($message) use ($from, $name, $to, $subject) {
-            $message->from($from, $name)->to($to)->subject($subject);
+        Mail::send($view, $data, function ($message) use ($to, $subject) {
+            $message->to($to)->subject($subject);
         });
     }
 
