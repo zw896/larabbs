36dae168e02f1a1e3ca8e7bcd7ed9c1182d36c36
Billy <billy@globaltllc.com>
2019-02-22 15:43:11 +1100

    11.2 fans list

--- app/Models/User.php
+++ app/Models/User.php
@@ -56,4 +56,35 @@ class User extends Authenticatable
     {
         return $this->hasMany(Status::class);
     }
+
+    public function followers()
+    {
+        return $this->belongsToMany(User::Class, 'followers', 'user_id', 'follower_id');
+    }
+
+    public function followings()
+    {
+        return $this->belongsToMany(User::Class, 'followers', 'follower_id', 'user_id');
+    }
+
+    public function follow($user_ids)
+    {
+        if ( ! is_array($user_ids)) {
+            $user_ids = compact('user_ids');
+        }
+        $this->followings()->sync($user_ids, false);
+    }
+
+    public function unfollow($user_ids)
+    {
+        if ( ! is_array($user_ids)) {
+            $user_ids = compact('user_ids');
+        }
+        $this->followings()->detach($user_ids);
+    }
+
+    public function isFollowing($user_id)
+    {
+        return $this->followings->contains($user_id);
+    }
 }
