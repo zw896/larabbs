37580a9633ab54b2ac0e018b80f09464afb6d060
Billy <billy@globaltllc.com>
2019-02-25 10:00:43 +1100

    11.5 follow btn

--- routes/web.php
+++ routes/web.php
@@ -23,7 +23,7 @@ Route::resource('statuses', 'StatusesController', ['only' => ['store', 'destroy'
 Route::get('/users/{user}/followings', 'UsersController@followings')->name('users.followings');
 Route::get('/users/{user}/followers', 'UsersController@followers')->name('users.followers');
 //
-
+//11.5
 Route::post('/users/followers/{user}', 'FollowersController@store')->name('followers.store');
 Route::delete('/users/followers/{user}', 'FollowersController@destroy')->name('followers.destroy');
 
